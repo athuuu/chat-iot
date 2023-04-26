e9a4acef85b7fe107cfbb5a54228e982fe97ebf7
athuuu <49942087+athuuu@users.noreply.github.com>
2023-04-26 16:18:20 +0200

    first commit web app

---
+++ index.js
@@ -0,0 +1,31 @@
+const { send } = require("process");
+
+const app = require("express")();
+
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) =>  {
+    res.sendFile(__dirname + "/index.html"); 
+});
+
+io.on("connection", (socket) => {
+    console.log(socket.id); 
+
+    socket.on("deconnecter", () => {
+        console.log("user deco ")
+    });
+
+    socket.on("chat_message", (msg) => {
+        console.log(msg);
+    });
+});
+
+
+
+
+http.listen(3000, () => {
+    console.log("serveur en marche")
+});
+
